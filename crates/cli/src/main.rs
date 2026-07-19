ee9eae623cbf946471b352e6ebc02e8c992c955f
Ze4ky <swt1me1962@gamil.com>
2026-07-19 16:34:55 +0800

    refactor: (cli)优化输出内容 1. 添加--enable_complete_display允许可选的输出请求的完整配置 2. 添加响应体的格式化输出

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -7,6 +7,9 @@ use hapi_core::{Api, model::RequestInfo};
 struct HeyApiCliArgs {
     #[arg(short, default_value = "./request.json")]
     file: String,
+
+    #[arg(long, default_value_t = false)]
+    enable_complete_display: bool,
 }
 
 #[tokio::main]
@@ -20,10 +23,12 @@ async fn main() {
     for request_info in request_info_vec.iter() {
         api_counter += 1;
         println!("{}. 请求描述: {}", api_counter, request_info.name);
-        println!("  请求URL: {}", request_info.url);
-        println!("  请求方法: {}", request_info.method);
-        println!("  请求头: {}", request_info.payload.headers);
-        println!("  请求体: {}", request_info.payload.body);
+        if args.enable_complete_display {
+            println!("  请求URL: {}", request_info.url);
+            println!("  请求方法: {}", request_info.method);
+            println!("  请求头: {}", request_info.payload.headers);
+            println!("  请求体: {}", request_info.payload.body);
+        }
     }
 
     let mut input = String::new();
@@ -40,18 +45,23 @@ async fn main() {
         num -= 1;
         input = String::new();
         let request_info = request_info_vec[num as usize].clone();
-        let response = match api_client.request(request_info).await {
+        let response = match api_client.request(request_info.clone()).await {
             Ok(resp) => resp,
             Err(err) => {
                 println!("请求错误: {}", err);
                 return;
             }
         };
+
         //println!("响应代码: {}", response.code);
-        println!("  响应头:");
+        println!("响应头:");
         for (k, v) in response.response_headers {
             println!("{}: {}", k, v)
         }
-        println!("  {}", response.response_body);
+        println!("响应体:");
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&response.response_body).unwrap()
+        );
     }
 }
